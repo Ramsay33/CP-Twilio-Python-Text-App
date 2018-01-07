8005f7fe218a1725aa103e327d5c35cb892dc470
Ramsay33 <35197570+Ramsay33@users.noreply.github.com>
2018-01-07 09:04:50 -0800

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "AC84aed0d047467fa9d38e20b4c5367d10"
+auth_token = "aef41ecd579481b405320fc5c4cce8fa"
+my_cell = "+15145892885"
+my_twilio = "+14387938656"
